bfa815e63ae5fbd76e577fbbc687bcae58a7017b
kazimahabubpg <88821836+kazimahabubpg@users.noreply.github.com>
2021-08-30 20:05:35 +0600

    Update index.js

--- instagram-master/src/index.js
+++ instagram-master/src/index.js
@@ -1,4 +1,9 @@
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '.App';
+import FirebaseContext from '.contextfirebase';
+import { firebase, FieldValue } from '.libfirebase';
+import '.stylesapp.css';
 
 ReactDOM.render(
   <FirebaseContext.Provider value={{ firebase, FieldValue }}>
